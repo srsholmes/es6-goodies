331d1eb6fdebb2b4101af0dd3e79b8e8e38ccf65
srsholmes <srsholmes@gmail.com>
2015-10-05 17:37:06 +0100

    range refactor

--- src/arrays/range.js
+++ src/arrays/range.js
@@ -1,5 +1,5 @@
 export default function range(start = 0 , end = start || 0, step = 1) {
   if (end == start || 0) start = 0;
   let length = Math.max(Math.ceil((end - start) / step), 0);
-  return [for (i of Array(length).keys()) i * step];
+  return Array.from({ length: length }, (v, k) => k * step);
 }
